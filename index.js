977c0dea3a86ca222200f1de4184288ba46bf492
AnnaAnnaNX <anna.anna.annanx@rambler.ru>
2023-05-29 04:24:57 +0300

    add

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
-const arr = [
-    {
-        id: 1,
+const obj = {
+    id: 10000,
         mainCategory: true,
         title: 'Обязательные для всех',
         circles: ko.observableArray([{color: 'pink'}, {color: 'yellow'}, {color: 'orange'}]),
@@ -8,48 +7,58 @@ const arr = [
         comment: 'Документы, обязательные для всех сотрудников',
         open: true,
         children: ko.observableArray([
-            {
-                id: 4,
-                mainCategory: false,
-                title: 'Паспорт',
-                circles: ko.observableArray([{color: 'blue'}]),
-                required: true,
-                comment: 'Для всех',
-            },
-            {
-                id: 5,
-                mainCategory: false,
-                title: 'ИНН',
-                circles: ko.observableArray([]),
-                required: true,
-                comment: 'Для всех',
-            },
-        ])
-    },   
-    {
-        id: 2,
-        mainCategory: true,
-        title: 'Обязательные для трудоустройства',
-        circles: ko.observableArray([]),
-        required: false,
-        comment: 'Документы, без которых невозможно трудоустройство человека на какую бы то ни было должность в компании вне зависимости от граж',
-        children: ko.observableArray([])
-    },    
-    {
-        id: 3,
-        mainCategory: true,
-        title: 'Специальные',
-        circles: ko.observableArray([]),
-        required: false,
-        comment: '',
-        children: ko.observableArray([])
-    },  
-]
+        {
+            id: 1,
+            mainCategory: true,
+            title: 'Обязательные для всех',
+            circles: ko.observableArray([{color: 'pink'}, {color: 'yellow'}, {color: 'orange'}]),
+            required: false,
+            comment: 'Документы, обязательные для всех сотрудников',
+            open: true,
+            children: ko.observableArray([
+                {
+                    id: 4,
+                    mainCategory: false,
+                    title: 'Паспорт',
+                    circles: ko.observableArray([{color: 'blue'}]),
+                    required: true,
+                    comment: 'Для всех',
+                },
+                {
+                    id: 5,
+                    mainCategory: false,
+                    title: 'ИНН',
+                    circles: ko.observableArray([]),
+                    required: true,
+                    comment: 'Для всех',
+                },
+            ])
+        },   
+        {
+            id: 2,
+            mainCategory: true,
+            title: 'Обязательные для трудоустройства',
+            circles: ko.observableArray([]),
+            required: false,
+            comment: 'Документы, без которых невозможно трудоустройство человека на какую бы то ни было должность в компании вне зависимости от граж',
+            children: ko.observableArray([])
+        },    
+        {
+            id: 3,
+            mainCategory: true,
+            title: 'Специальные',
+            circles: ko.observableArray([]),
+            required: false,
+            comment: '',
+            children: ko.observableArray([])
+        },  
+    ])
+}
 
 function ViewModel() {
     var self = this;
 
-    self.categories = ko.observableArray(arr);
+    self.categories = obj.children;
 
     self.relocate = function(id, parentId, newParentId, newPrevId) {
         id = 1; parentId=null; newParentId=null; newPrevId = 3;
